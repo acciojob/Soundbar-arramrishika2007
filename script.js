83212cdaef78f5f795e68e880fafe7394dde9986
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-07-04 19:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const sounds = [
+  "applause",
+  "boo",
+  "gasp",
+  "tada",
+  "victory",
+  "wrong"
+];
+
+const buttons = document.getElementById("buttons");
+
+let currentAudio = null;
+
+sounds.forEach(sound => {
+  const button = document.createElement("button");
+  button.className = "btn";
+  button.innerText = sound;
+
+  button.addEventListener("click", () => {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio.play();
+  });
+
+  buttons.appendChild(button);
+});
+
+const stopButton = document.createElement("button");
+stopButton.className = "stop";
+stopButton.innerText = "stop";
+
+stopButton.addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
+
+buttons.appendChild(stopButton);
